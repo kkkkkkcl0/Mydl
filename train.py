10f03524eee658500c8afcb823e060cc3b376dcf
CHENGYU LIU <1619962525@qq.com>
2026-09-01 00:16:12 +0800

    init

---
+++ train.py
@@ -0,0 +1,11 @@
+def train(net, train_iter, loss, optimier, epochs):
+    num_loss = 0
+    num_y = 0
+    #训练核心部分
+    for epoch in epochs:
+        for X, y in train_iter:
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimier.zero_grad()
+            l.backward()
+            optimier.step()
