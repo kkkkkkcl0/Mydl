b981d77a8d612d19c778ed567779d0491d257dac
Kcl0 <liuchengyu378@gmail.com>
2026-09-01 11:43:58 +0800

    0.1.0

--- src/mydl/train.py
+++ src/mydl/train.py
@@ -1,8 +1,8 @@
 def train(net, train_iter, loss, optimizer, epochs):
-    total_loss = 0
-    total_num = 0
-    #训练核心部分
+        #训练核心部分
     for epoch in range(epochs):
+        total_loss = 0
+        total_num = 0
         for X, y in train_iter:
             y_hat = net(X)
             l = loss(y_hat, y)
@@ -10,7 +10,8 @@ def train(net, train_iter, loss, optimizer, epochs):
             l.backward()
             optimizer.step()
 
-        total_loss += l.item() * y.numel()
-        total_num += y.numel()
+            total_loss += l.item() * y.numel()
+            total_num += y.numel()
         print(f'epoch {epoch + 1}, loss {total_loss / total_num:.4f}')
+        # 我觉得这次的改动是值得被品鉴的，它涉及到缩进导致的循环问题，就是这个计数到底是该在哪一个循环里面？
             
